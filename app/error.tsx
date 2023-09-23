a4907e8d0443c04bee60c2239023bd2606f53420
dp codes <124174236+Irasubiza1@users.noreply.github.com>
2023-09-23 17:20:42 +0200

    divin9

---
+++ app/error.tsx
@@ -0,0 +1,8 @@
+"use client";
+const Error = () => {
+   return (
+      <div className='h-full text-center font-semibold text-muted-foreground'>SomeThing Went Wrong!</div>
+   );
+}
+
+export default Error;
