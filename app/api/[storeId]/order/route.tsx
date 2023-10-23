542331483ba9010b68ad2f41b0d325f61f065ced
dp <124174236+DivinPrince@users.noreply.github.com>
2023-10-23 11:56:37 +0200

    init

--- app/api/[storeId]/order/route.tsx
+++ app/api/[storeId]/order/route.tsx
@@ -5,50 +5,53 @@ export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
-  const { productIds, info } = await req.json();
-
+  
   try {
+    const body = await req.json();
+
+    const { productIds, info } = body
     if (!productIds || productIds.length === 0) {
-    return new NextResponse("Product ids are required", { status: 400 });
-  }
-
-  const products = await prismadb.product.findMany({
-    where: {
-      id: {
-        in: productIds,
-      },
-    },
-  });
-
-  const district = await prismadb.district.findFirst({
-    where:{
-      id: info.district
+      return new NextResponse("Product ids are required", { status: 400 });
     }
-  })
 
-  if (!district) {
-    return new NextResponse('')
-  }
-
-  const order = await prismadb.order.create({
-    data: {
-      storeId: params.storeId,
-      isPaid: false,
-      phone: info.phoneNumber,
-      districtId: district.id,
-      orderItems: {
-        create: productIds.map((productId: string) => ({
-          product: {
-            connect: {
-              id: productId,
-            },
-          },
-        })),
+    const products = await prismadb.product.findMany({
+      where: {
+        id: {
+          in: productIds,
+        },
       },
-    },
-  });
-  return NextResponse.json(order);
+    });
+
+    const district = await prismadb.district.findFirst({
+      where: {
+        id: info.district,
+      },
+    });
+
+    if (!district) {
+      return new NextResponse("district is required", { status: 403 });
+    }
+
+    const order = await prismadb.order.create({
+      data: {
+        storeId: params.storeId,
+        isPaid: false,
+        phone: info.phoneNumber,
+        districtId: district.id,
+        orderItems: {
+          create: productIds.map((productId: string) => ({
+            product: {
+              connect: {
+                id: productId,
+              },
+            },
+          })),
+        },
+      },
+    });
+    return NextResponse.json(order);
   } catch (error) {
-    console.log(error);   
+    console.log('[ORDER_ERR]', error);
+    return new NextResponse("Internal error", { status: 500 });
   }
 }
